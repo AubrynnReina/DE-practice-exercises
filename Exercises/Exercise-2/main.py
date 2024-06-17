1aa5c2c19330d201bf17a182fb767b9242edd0fe
Anh Nguyễn Trí <78487345+ChariskYuri@users.noreply.github.com>
2024-06-17 09:55:15 +0700

    Finish Ex2

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,8 +1,11 @@
 import requests
-import pandas
+import pandas as pd
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 import os
+import glob
+from pathlib import Path
+import re
 
 
 def get_files_to_download(url, timestamp) -> list[str]:
@@ -63,7 +66,7 @@ def download_from_uri(uri, save_dir) -> bool:
     
     print('\nFetching the file...')
     response = requests.get(uri)
-    
+
     # Check if the uri is valid
     if response.status_code != 200:
         print('The URI is not valid.')
@@ -81,18 +84,59 @@ def download_from_uris(uris):
         download_from_uri(uri=uri, save_dir=save_dir)
 
 
+def write_concatenated_file_from_source(file_name, source, destination) -> Path:
+    dfs = []
+    csv_files = Path('./').glob(f'{source}/*.csv')
+
+    for file in tqdm(csv_files):
+        data = pd.read_csv(file)
+        # .stem is method for pathlib objects to get the filename w/o the extension
+        data['file'] = file.stem
+        dfs.append(data)
+
+    df = pd.concat(dfs, ignore_index=True)
+    df.to_csv(f'{destination}/{file_name}', index=False)
+    return Path(f'{destination}/{file_name}')
+
+
+def get_record_with_max_required_col(in_file, required_col, destination, out_file):
+
+    df = pd.read_csv(in_file)
+    numeric_required_data = pd.to_numeric(df[required_col], errors='coerce')
+    record_with_max_required_data = df.iloc[numeric_required_data.idxmax()]
+    record_with_max_required_data.to_csv(f'{destination}/{out_file}', index=True)
+
+
 def main():
     timestamp = '2024-01-19 10:45'
     url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+    required_col = 'HourlyDryBulbTemperature'
+    concatenated_file_name = 'concatenated.csv'
+    source_dir = './downloads'
+    destination_dir = './data'
+    result_file_name = 'result.csv'
+
     
     files_to_download = get_files_to_download(url=url, timestamp=timestamp)
     create_new_directory('./downloads')
 
     uris = make_uris_from_file_names(url=url, file_names=files_to_download)
-    print(uris)
     download_from_uris(uris=uris)
 
+    create_new_directory('./data')
+    file_path = write_concatenated_file_from_source(
+        file_name=concatenated_file_name, 
+        source=source_dir, 
+        destination=destination_dir
+    )
 
+    get_record_with_max_required_col(
+        in_file=file_path,
+        required_col=required_col, 
+        destination=destination_dir, 
+        out_file=result_file_name
+    )
+    
     pass
 
 
