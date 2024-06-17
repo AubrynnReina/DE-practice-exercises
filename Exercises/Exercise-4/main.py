d048be9eab1bd8033094d3daffd073890fdae56e
Anh Nguyễn Trí <78487345+ChariskYuri@users.noreply.github.com>
2024-06-17 15:48:43 +0700

    Finish Ex4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,8 +1,37 @@
-import boto3
+from pathlib import Path
+import json
+import pandas as pd
+from tqdm import tqdm
+
+
+def flatten_json_files(json_files) -> pd.DataFrame:
+
+    df = pd.DataFrame()
+    for file in tqdm(json_files):
+        with open(file, 'r+') as f:
+            data = json.load(f)
+            flatten_json = pd.json_normalize(data)
+
+            # Add the 'file' column for backtracking
+            flatten_json['file'] = f'{file.stem}.json'
+
+            df = pd.concat([df, flatten_json], ignore_index=True)
+
+    return df
 
 
 def main():
-    # your code here
+
+    destination_dir = './data'
+    output_file = 'result.csv'
+    # Get all json files
+    json_files = Path('./').rglob('*.json')
+
+    df = flatten_json_files(json_files=json_files)
+
+    # Save to ./data
+    df.to_csv(f'{destination_dir}/{output_file}', index=False)
+
     pass
 
 
